c8d0f13a824aa6c12ce3a23ec7da1ea8c103a9b0
Jon Ablondi <jonnyablondi@hotmail.com>
2017-11-03 15:21:56 +1100

    shrine added

---
+++ config/initializers/shrine.rb
@@ -0,0 +1,11 @@
+require "cloudinary"
+require "shrine/storage/cloudinary"
+
+Cloudinary.config(
+  
+)
+
+Shrine.storages = {
+  cache: Shrine::Storage::Cloudinary.new(prefix: "cache"), # for direct uploads
+  store: Shrine::Storage::Cloudinary.new(prefix: "store")
+}
