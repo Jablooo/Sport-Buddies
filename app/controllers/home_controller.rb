2dd2a603abbcad056dbb359271012be0cd5508fb
Jon Ablondi <jonnyablondi@hotmail.com>
2017-11-04 07:40:47 +1100

    added params of profile to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,26 @@
 class HomeController < ApplicationController
+
   def index
+    @profiles = Profile.all
+  end
+  
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def profile_params
+    params.require(:item).permit(
+      :avatar,
+      :first_name,
+      :last_name,
+      :sex,
+      :DOB,
+      :postcode,
+      :user_id
+    )
   end
 end
